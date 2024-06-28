e920f8806c245508b3b364851e689e35c35b1308
pratham0709 <prathameshjadhav792@gmail.com>
2024-06-28 16:27:17 +0530

    new changes

---
+++ BlogApp -  Scratch/models/likeModel.js
@@ -0,0 +1,19 @@
+
+// Import
+const mongoose = require("mongoose");
+
+// Defining the Schema 
+const likeSchema = new mongoose.Schema({
+    post: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Post",
+    },
+    user: {
+        type: String,
+        required : true,
+    }
+
+})
+
+// exports
+module.exports = mongoose.model("Post", likeSchema);
