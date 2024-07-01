dfd9cef510c18de46abcfb1f4db426747b1e8284
pratham0709 <prathameshjadhav792@gmail.com>
2024-07-01 19:48:19 +0530

    New File Changes

---
+++ Codes/BD + Express - 2/Lecture 1/authApp/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {console.log("DB Connected Sucessfully")})
+    .catch( (error) => {
+        console.log("DB Connection Issues");
+        console.error(error);
+
+        process.exit(1);
+    })
+}
---
+++ Codes/BD + Express - 2/Lecture 1/authApp/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+
+require("./config/database").connect();
+
+// import routes
+const user = require("./routes/user");
+app.use("api/v1", user);
+
+// activate server
+app.listen(PORT, () =>{
+    console.log(`App is listening at ${PORT}`);
+})
