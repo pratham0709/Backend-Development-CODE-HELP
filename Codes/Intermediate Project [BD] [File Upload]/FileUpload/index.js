29f349f5f6738e2c3d66276b00b605e771e4a16c
pratham0709 <prathameshjadhav792@gmail.com>
2024-07-06 23:09:51 +0530

    new changes

--- Codes/Intermediate Project [BD] [File Upload]/FileUpload/index.js
+++ Codes/Intermediate Project [BD] [File Upload]/FileUpload/index.js
@@ -1,4 +1,17 @@
+// app crete
 const express = require("express");
 const app = express();
 
+// Port find krna hai
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+// middleware add krna hai
+app.use(express.json());
+
+// db se connect krna hai
+
+// cloud se connect krna hai
+
+// api route mount krna hai
 
